4cc930fec8176c29186f12cc5e059720c7682c13
Chonnisa Thiembundit <tiewwa_haha@hotmail.com>
2018-11-08 09:44:07 +0700

    add deleteMapping

--- src/main/java/com/business/auth/user/UserController.java
+++ src/main/java/com/business/auth/user/UserController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,7 @@ public class UserController {
         return res;
     }
 
+    @DeleteMapping("/{id}")
     public Map<String, Object> deleteUser(@PathVariable Long id) {
         Map<String, Object> res = new HashMap<>();
         if (userRepository.existsById(id)) {
